983ae1e17784fee0a2ee528d7bf601c9667f2a9e
David Space <genarodavid@gmail.com>
2024-03-27 15:53:42 -0400

    deleted redudants files

--- addons/awesome_input_icons/editor/pluginLoader.gd
+++
@@ -1,8 +0,0 @@
-@tool
-extends EditorPlugin
-
-const UPDATE_BUTTON_SCENE = preload ("res://addons/awesome_input_icons/editor/update_button.tscn")
-var docker_scene = null
-
-func _enter_tree():
-	docker_scene = UPDATE_BUTTON_SCENE.instantiate()
