012007c5f209d4f4b7ebf656a956b4c750b16174
David Space <genarodavid@gmail.com>
2026-02-28 13:52:44 -0400

    made the playground code a bit prettier

--- playground.gd
+++ playground.gd
@@ -3,22 +3,24 @@ extends Node2D
 @onready var sprite: Sprite2D = $Sprite2D
 
 
-#region Nuevo Code Region
 func _input(event):
-	if (
-		event is InputEventKey and event.is_released()
-	) or (
-		is_correct_joypad_input(event) or is_correct_mouse_input(event)
-	):
+	var is_key_event := event is InputEventKey
+	var is_joypad_input := is_correct_joypad_input(event)
+	var is_mouse_input := is_correct_mouse_input(event)
+
+	if (is_key_event and event.is_pressed()) or (is_joypad_input or is_mouse_input):
 		sprite.texture = InputIcon.get_icon_by_event(event)
-#endregion
 
 
-func is_correct_joypad_input(event: InputEvent):
+func is_correct_joypad_input(event: InputEvent) -> bool:
 	var deadzone := 0.5
-	return event is InputEventJoypadButton or (event is InputEventJoypadMotion and absf(event.axis_value) >= deadzone)
+	var is_joypad_button := event is InputEventJoypadButton
+	var is_joypad_motion := event is InputEventJoypadMotion
+	return is_joypad_button or (is_joypad_motion and absf(event.axis_value) >= deadzone)
 
 
-func is_correct_mouse_input(event: InputEvent):
+func is_correct_mouse_input(event: InputEvent) -> bool:
 	var deadzone: int = 10
-	return event is InputEventMouseButton or (event is InputEventMouseMotion and abs(event.relative.length()) >= deadzone)
+	var is_mouse_button := event is InputEventMouseButton
+	var is_mouse_motion := event is InputEventMouseMotion
+	return is_mouse_button or (is_mouse_motion and abs(event.relative.length()) >= deadzone)
