8bfd8196e4e606f1c095e0b32adf47c0ba80e36e
David Space <genarodavid@gmail.com>
2026-02-28 12:52:08 -0400

    ran the default updater to 4.5

--- playground.gd
+++ playground.gd
@@ -2,20 +2,22 @@ extends Node2D
 
 @onready var sprite: Sprite2D = $Sprite2D
 
+
 #region Nuevo Code Region
 func _input(event):
 	if (
 		event is InputEventKey and event.is_released()
-		) or (
+	) or (
 		is_correct_joypad_input(event) or is_correct_mouse_input(event)
 	):
 		sprite.texture = InputIcon.get_icon_by_event(event)
-
 #endregion
+
 
 func is_correct_joypad_input(event: InputEvent):
 	var deadzone := 0.5
 	return event is InputEventJoypadButton or (event is InputEventJoypadMotion and absf(event.axis_value) >= deadzone)
+
 
 func is_correct_mouse_input(event: InputEvent):
 	var deadzone: int = 10
